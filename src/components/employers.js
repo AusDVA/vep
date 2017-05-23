c1dd17acba020cd0d04a26791f26b770437ce275
shatheesh <nakesuparan.shatheesh@dva.gov.au>
2017-05-23 14:32:01 +1000

    employers page updated

--- src/components/employers.js
+++ src/components/employers.js
@@ -5,7 +5,7 @@ export default class Employers extends React.Component {
   render() {
     return (
       <div>
-        <header className="page-header" id="top">
+        <header className="newsfeed" id="top">
           <div className="container">
             <div className="row">
               <div className="col-sm-12">
@@ -23,22 +23,22 @@ export default class Employers extends React.Component {
               <ul className="list-unstyled" id="sidebar">
               <li className="list-unstyled-header"> Contents </li>
                   <li>
-                    <a href="#sec0">Benefits of employing veterans</a>
+                    <a href="#Benefits">Benefits of employing veterans</a>
                   </li>
                   <li>
-                    <a href="#sec1">Industry Advisory Committee on Veterans’ Employment</a>
+                    <a href="#Industry">Industry Advisory Committee on Veterans’ Employment</a>
                   </li>
                   <li>
-                    <a href="#sec2">Ex-service Organisation Industry Partnership Register</a>
+                    <a href="#ExService">Ex-service Organisation Industry Partnership Register</a>
                   </li>
                   <li>
-                    <a href="#sec3">Department of Employment initiatives</a>
+                    <a href="#Department">Department of Employment initiatives</a>
                   </li>
                   <li>
-                    <a href="#sec4">Prime Minister’s Veterans’ Employment Annual Awards</a>
+                    <a href="#PrimeMinister">Prime Minister’s Veterans’ Employment Annual Awards</a>
                   </li>
                   <li>
-                    <a href="#sec5">Resources for employers</a>
+                    <a href="#EmployerResources">Resources for employers</a>
                   </li>
                   <li>
                     <a href="#sec">Resources-general</a>
@@ -53,7 +53,7 @@ export default class Employers extends React.Component {
                 <p>The Program aims to promote awareness of the benefits of employing veterans to ensure that Australian businesses can reap the benefits of their service in the ADF.</p>
 
 
-                <h2 id="sec0">Benefits of employing veterans</h2>
+                <h2 id="Benefits">Benefits of employing veterans</h2>
 
                 <p>Members of the ADF gain an extensive range of skills, abilities, recognised behaviours and attributes that make them valuable employees in the civilian workforce.
                    They are good at solving problems, leading teams, and working under pressure.  They retain core ADF values such as honesty, honour, initiative, integrity, respect and loyalty.
@@ -76,30 +76,30 @@ export default class Employers extends React.Component {
                    for example mechanics, health professionals, carpenters and drivers.
                 </p>
 
-                <h2 id="sec1">Industry Advisory Committee on Veterans’ Employment </h2>
+                <h2 id="Industry">Industry Advisory Committee on Veterans’ Employment </h2>
                  <p>The Industry Advisory Committee on Veterans’ Employment has been established under the Program to develop and provide advice on practical measures
                    to embed veterans’ employment strategies into the recruitment practices of Australian businesses.
                  </p>
 
-                 <h2 id="sec2">Ex-service Organisation Industry Partnership Register</h2>
+                 <h2 id="ExService">Ex-service Organisation Industry Partnership Register</h2>
                  <p>Employers will also benefit from the establishment of the Ex-service Organisation Industry Partnership Register. </p>
                  <p>This Register will enable ex-service organisations to register their interest in partnering with industry on projects to promote the employment of veterans.
                     Employers will be able to access this information when they are seeking a partner to assist in the creation of employment opportunities for veterans, including in regional and remote areas of the country.
                   </p>
                   <p>The Register will be launched later in 2017.</p>
 
-                  <h2 id="sec3">Department of Employment initiatives</h2>
+                  <h2 id="Department">Department of Employment initiatives</h2>
                   <p>Employers who are interested in hiring veterans can now indicate that defence force experience is desirable when they advertise a job on the
                      Australian Government’s <a href="http://www.jobactive.gov.au/">jobactive website</a>, by selecting the optional ‘defence force experience desirable’ flag.
                      More information on how to advertise vacancies is available on the Resources for Employers page.
                   </p>
 
-                  <h2 id="sec4">Prime Minister’s Veterans’ Employment Annual Awards</h2>
+                  <h2 id="PrimeMinister">Prime Minister’s Veterans’ Employment Annual Awards</h2>
                   <p>The achievements of companies in creating employment opportunities for veterans will be recognised through the Prime Minister’s Veterans’ Employment Annual Awards.</p>
                   <p>Nominations for the awards will open later in 2017, with the first awards to be presented in late 2017.</p>
 
 
-                 <h2 id="sec5">Resources for employers</h2>
+                 <h2 id="EmployerResources">Resources for employers</h2>
 
                  <h3>jobactive </h3>
                  <p>jobactive is the Australian Government’s way to get more Australians into work by connecting job seekers with employers. </p>
