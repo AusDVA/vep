0442b30a6cc8f74766c3d2466dc226abbbcdbe98
Callum McKenna <my.nirvana@gmail.com>
2017-05-23 13:18:42 +1000

    Updated the About page

--- src/components/about.js
+++ src/components/about.js
@@ -8,10 +8,6 @@ export default class About extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col-sm-12">
-                <span>
-                  <h3>Prime Minister’s Veterans’ Employment Program</h3>
-                </span>
-                <h1 id="content">About the program</h1>
               </div>
             </div>
           </div>
@@ -20,54 +16,51 @@ export default class About extends React.Component {
           <div className="body-content container">
             <div className="row">
               <div className="col-md-7" id="mainCol">
-                <p>The Prime Minister's Veterans' Employment Program demonstrates the importance the Government places on raising awareness with employers,
-                   both private and public sector, of the value and unique experience of our veterans.</p>
-                <p>Members of the Australian Defence Force (ADF) gain an extensive range of skills, abilities, recognised behaviours and attributes that
-                   make them valuable employees in the civilian workforce. They possess skills such as problem-solving, leadership, team work and the ability
-                   to work in high-pressure environments, just to name a few. Many former members continue to live by core ADF values such as honesty, honour,
-                   initiative, integrity, respect and loyalty. These attributes are an asset to any employer.</p>
-                <p>The Program will also work with industry to embed veterans' employment programs in their recruitment strategies.</p>
-                <h2>Initiatives</h2>
-                <h4>Industry Advisory Committee on Veterans' Employment</h4>
-                <p>The <a href="">Industry Advisory Committee </a>
-                   on Veterans' Employment will develop and provide advice on practical measures to embed veterans' employment strategies into the recruitment
-                  practices of Australian business. The Committee will play a role in the broader promotion of the skills and professional attributes that veterans
-                  have to offer employers. Advisory Committee members will be known as 'Veteran Employment Ambassadors'. Interested firms are encouraged to apply to be members
-                  of the Advisory Committee. The Government will assist to set up the Advisory Committee which will transition to become an industry led committee.
-                  The first meeting will occur in the first half of 2017 and will be chaired by the Minister for Veterans' Affairs and Defence Personnel.
-                  Companies can register their interest by emailing <a href="#">veteransemployment@dva.gov.au</a>
-                </p>
-                <h4>The Prime Minister's Veterans' Employment Annual Awards</h4>
-                <p>The Prime Minister's Veterans' Employment Annual Awards will recognise the achievements of small, medium and large companies in creating employment
-                   opportunities for veterans. The criteria for the awards will be developed in consultation with the Industry Advisory Committee.</p>
-                <h4>Ex-Service Organisation Industry Partnership Register</h4>
-                <p>Ex-service organisations are invited to register their interest in partnering with industry on projects to promote the employment of veterans.
-                   Industry will be able to access this information when they are seeking a partner to assist in the creation of employment opportunities for veterans,
-                   including in regional and remote areas of the country. Ex-service organisations can register their interest by emailing <a href="#">ESOindustryregister@dva.gov.au</a>
-                </p>
-                <h4>Department of Defence and Department of Veterans' Affairs initiatives</h4>
-                <ul>
-                  <li>Improvements to the transition process for separating members of the Australian Defence Force (ADF) that will include ensuring all individuals leave Defence with
-                      separation documentation, such as their training records, and an individual transition plan.</li>
-                  <li>Tailored transition coaching services for all separating members.</li>
-                  <li>Reinstating surveys of former ADF personnel post-separation to assess the effectiveness of ADF transition programs and inform future initiatives.</li>
-                  <li>Working with civilian authorities to align military qualifications with civilian qualifications.</li>
-                  <li>Improvements to vocational rehabilitation services for veterans with an ADF-related injury or illness.</li>
-                </ul>
-                <h4>Australian Public Service (APS) initiatives</h4>
-                <ul>
-                  <li>Launch of the new APSJobs website in 2017 will include specific information for veterans seeking employment in the APS.</li>
-                  <li>Development of a tool that aligns ADF ranks to APS classifications.</li>
-                  <li>Introduction of a toolkit that outlines information for veterans on working in the APS.</li>
-                </ul>
-                <h4>Department of Employment initiatives</h4>
-                <ul>
-                  <li>Creation of an information page for veterans on the Government's Jobactive website.</li>
-                  <li>Creation of an optional 'defence force experience desirable' flag for vacancies listed on the jobactive website that will enable job seekers to search for vacancies
-                      for which former ADF members would be well suited and then to apply directly to the employer.</li>
-                  <li>Development of a stronger partnership with the Department of Defence to further enhance the Career Transition Assistance Scheme for separating ADF members,
-                      including employment planning advice and labour market information.</li>
-                </ul>
+                <h1> About the Veterans’ Employment Program </h1>
+                <p>On 17 November 2016 the Prime Minister announced six initiatives under the Program.</p>
+
+                <h2>Industry Advisory Committee on Veterans’ Employment</h2>
+                <p>The <a href="#">Industry Advisory Committee on Veterans’ Employment (IAC)</a> will be established to develop and
+                  provide advice on practical measures to embed veterans’ employment strategies into the recruitment
+                  practices of Australian businesses.  The Committee will play a role in the broader promotion of the
+                  skills and professional attributes that veterans have to offer employers.</p>
+
+                <h2>The Prime Minister’s Veterans’ Employment Annual Awards</h2>
+                <p>The Prime Minister’s Veterans’ Employment Annual Awards will recognise the achievements of companies
+                  in creating employment opportunities for veterans.  The criteria for the awards will be developed in
+                  consultation with the Industry Advisory Committee on Veterans’ Employment.</p>
+                <p>The inaugural awards will be presented in late 2017.</p>
+
+                <h2>Ex-Service Organisation Industry Partnership Register</h2>
+                <p>Ex-service organisations will be able to register their interest in partnering with industry on
+                  projects to promote the employment of veterans.  Industry will be able to access this information
+                  when they are seeking a partner to assist in the creation of employment opportunities for veterans,
+                  including in regional and remote areas of the country.</p>
+                <p>Ex-service organisations can register their interest by
+                  emailing <a href="mailto:ESOindustryregister@dva.gov.au">ESOindustryregister@dva.gov.au</a></p>
+
+                <h2>Department of Defence and Department of Veterans’ Affairs initiatives</h2>
+                <p>The Departments of Defence (Defence) and Veterans’ Affairs (DVA) will continue to improve the transition process for separating members of the Australian Defence Force (ADF). </p>
+                <p>Separating members of the ADF will receive separation documentation, including training records.  They will also receive an individual transition plan and tailored transition coaching services.  Surveys of former ADF personnel post-separation will be reinstated to assess the effectiveness of ADF transition programs and inform future initiatives, and work will continue on aligning military qualifications with civilian qualifications.</p>
+                <p>Vocational rehabilitation services for veterans with an ADF-related injury or illness will also be improved.</p>
+
+                <h2>Australian Public Service (APS) initiatives</h2>
+                <p>A new <a href="https://www.apsjobs.gov.au/" target="_blank">APSjobs website</a> will be launched in 2017 and will include specific information
+                  for veterans seeking employment in the APS.  An application that aligns ADF ranks to APS
+                  classifications and a toolkit that provides information to veterans about working in the APS
+                  are also being developed.</p>
+
+                <h2>Department of Employment initiatives</h2>
+                <p>The Australian Government’s <a href="https://jobactive.gov.au/" target="_blank">jobactive website</a> will
+                  include an <a href="https://jobsearch.gov.au/jobseeker-info/employment-assistance-for-veteranshttps:/jobsearch.gov.au/jobseeker-info/employment-assistance-for-veterans" target="_blank">information page for veterans</a> and
+                  an optional ‘defence force experience desirable’
+                  flag for vacancies listed on the website.  This will enable former ADF members seeking a job
+                  to <a href="https://jobsearch.gov.au/job/search/ex-adf" target="_blank">search for vacancies</a> for
+                  which they would be well suited and allow them to apply directly to the employer.</p>
+                <p>The Department of Employment will also develop a stronger partnership with the Department of Defence
+                  to enhance the Career Transition Assistance Scheme for separating ADF members, including the
+                  provision of employment planning advice and labour market information.</p>
+
                 <p>
                   <a className="" href="#">Top <span className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a>
                 </p>
