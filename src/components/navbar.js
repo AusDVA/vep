fb95fd867b38566216d731a479cf212718e1e268
Callum McKenna <my.nirvana@gmail.com>
2017-05-23 11:26:49 +1000

    Changes to top navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,16 +13,23 @@ export default class NavBar extends React.Component {
       <div className="container">
         <div className="row">
           <div className="logo col-md-6"><img alt="Logo" src={GOVLogo}/></div>
+          <div className="col-md-6">
+              <nav id="top-nav">
+                <ul className="list-inline pull-right">
+                  <li><NavLink to="/about">About</NavLink></li>
+                  <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+              </nav>
+          </div>
         </div>
         <nav>
           <ul className="list-inline">
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/veterans">Veterans</NavLink></li>
             <li><NavLink to="/employers">Employers</NavLink></li>
-            <li><NavLink to="/awards">Awards</NavLink></li>
             <li><NavLink to="/esos">Ex-service organisations</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/iac">Industry Advisory Committee</NavLink></li>
+            <li><NavLink to="/awards">Awards</NavLink></li>
           </ul>
         </nav>
       </div>
