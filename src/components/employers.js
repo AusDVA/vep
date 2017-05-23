cbf8e9b7a2096e4a274ac288b2bdb8ff2ff6a22c
shatheesh <nakesuparan.shatheesh@dva.gov.au>
2017-05-23 13:32:33 +1000

    Employers page updated

--- src/components/employers.js
+++ src/components/employers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GeneralResourses from "./generalResources";
 export default class Employers extends React.Component {
 
   render() {
@@ -22,19 +23,25 @@ export default class Employers extends React.Component {
               <ul className="list-unstyled" id="sidebar">
               <li className="list-unstyled-header"> Contents </li>
                   <li>
-                    <a href="#sec0">Finding jobs</a>
+                    <a href="#sec0">Benefits of employing veterans</a>
                   </li>
                   <li>
-                    <a href="#sec1">Planning and rehabilitating</a>
+                    <a href="#sec1">Industry Advisory Committee on Veterans’ Employment</a>
                   </li>
                   <li>
-                    <a href="#sec2">Finances</a>
+                    <a href="#sec2">Ex-service Organisation Industry Partnership Register</a>
                   </li>
                   <li>
-                    <a href="#sec3">State government support</a>
+                    <a href="#sec3">Department of Employment initiatives</a>
                   </li>
                   <li>
-                    <a href="#sec4">Resourses-general</a>
+                    <a href="#sec4">Prime Minister’s Veterans’ Employment Annual Awards</a>
+                  </li>
+                  <li>
+                    <a href="#sec5">Resources for employers</a>
+                  </li>
+                  <li>
+                    <a href="#sec">Resources-general</a>
                   </li>
                 </ul>
 
@@ -46,7 +53,7 @@ export default class Employers extends React.Component {
                 <p>The Program aims to promote awareness of the benefits of employing veterans to ensure that Australian businesses can reap the benefits of their service in the ADF.</p>
 
 
-                <h2>Benefits of employing veterans</h2>
+                <h2 id="sec0">Benefits of employing veterans</h2>
 
                 <p>Members of the ADF gain an extensive range of skills, abilities, recognised behaviours and attributes that make them valuable employees in the civilian workforce.
                    They are good at solving problems, leading teams, and working under pressure.  They retain core ADF values such as honesty, honour, initiative, integrity, respect and loyalty.
@@ -69,30 +76,40 @@ export default class Employers extends React.Component {
                    for example mechanics, health professionals, carpenters and drivers.
                 </p>
 
-                <h2>Industry Advisory Committee on Veterans’ Employment </h2>
+                <h2 id="sec1">Industry Advisory Committee on Veterans’ Employment </h2>
                  <p>The Industry Advisory Committee on Veterans’ Employment has been established under the Program to develop and provide advice on practical measures
                    to embed veterans’ employment strategies into the recruitment practices of Australian businesses.
                  </p>
 
-                 <h2>Ex-service Organisation Industry Partnership Register</h2>
+                 <h2 id="sec2">Ex-service Organisation Industry Partnership Register</h2>
                  <p>Employers will also benefit from the establishment of the Ex-service Organisation Industry Partnership Register. </p>
                  <p>This Register will enable ex-service organisations to register their interest in partnering with industry on projects to promote the employment of veterans.
                     Employers will be able to access this information when they are seeking a partner to assist in the creation of employment opportunities for veterans, including in regional and remote areas of the country.
                   </p>
                   <p>The Register will be launched later in 2017.</p>
 
+                  <h2 id="sec3">Department of Employment initiatives</h2>
+                  <p>Employers who are interested in hiring veterans can now indicate that defence force experience is desirable when they advertise a job on the
+                     Australian Government’s <a href="http://www.jobactive.gov.au/">jobactive website</a>, by selecting the optional ‘defence force experience desirable’ flag.
+                     More information on how to advertise vacancies is available on the Resources for Employers page.
+                  </p>
+
+                  <h2 id="sec4">Prime Minister’s Veterans’ Employment Annual Awards</h2>
+                  <p>The achievements of companies in creating employment opportunities for veterans will be recognised through the Prime Minister’s Veterans’ Employment Annual Awards.</p>
+                  <p>Nominations for the awards will open later in 2017, with the first awards to be presented in late 2017.</p>
 
 
-                <h2>Advertising jobs to our veterans</h2>
+                 <h2 id="sec5">Resources for employers</h2>
 
-                <p>The Australian Government provides a free jobs board on the Jobactive website, open to all employers, to advertise their vacancies.
-                   It has been enhanced to allow employers to indicate that they prefer applicants with ADF experience.
-                   Veterans can access the site and search specifically for jobs with 'Defence Force Experience Desirable'.
+                 <h3>jobactive </h3>
+                 <p>jobactive is the Australian Government’s way to get more Australians into work by connecting job seekers with employers. </p>
+                 <p>Employers who are interested in hiring veterans can indicate that defence force experience is desirable when they advertise a job on the
+                    Australian Government’s <a href="http://www.jobactive.gov.au/">jobactive website</a>, by selecting the optional ‘defence force experience desirable’ flag. Employers can also approach local jobactive,
+                    Transition to Work or Disability Employment Service providers to list vacancies targeting veterans on their behalf.
                 </p>
-                <p>Employers can also approach local Jobactive, Transition to Work or Disability Employment Service providers to list vacancies aimed at veterans on their behalf.
-                   Employers need to register online at jobactive.gov.au, and instructions about listing vacancies and using the 'Defence Force Experience Desirable' flag are available.
-                </p>
+                <p> A guide has been developed to assist employers to list vacancies using the ‘defence force experience desirable’ flag.</p>
 
+                <GeneralResourses/>
                 <p>
                   <a className="" href="#">Top <span className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span></a>
                 </p>
