e7f6587f94cf73eaa7424b4aca90a32d6850f58d
Jack <jack.dillon266@gmail.com>
2017-05-24 16:49:44 +1000

    read more on homepage. Show/hide div.

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import News from './news';
-import {Link} from 'react-router-dom';
+import React from "react";
+import News from "./news";
+import {Link} from "react-router-dom";
+import ToggleDisplay from "react-toggle-display";
+
+
 export default class Home extends React.Component {
+
+   constructor() {
+    super();
+    this.state = { show: false };
+  }
+ 
+  handleClick() {
+    this.setState({
+      show: !this.state.show
+    });
+  }
 
   render() {
     return (
@@ -11,9 +25,15 @@ export default class Home extends React.Component {
             <div className="description">
               <div className="row">
                 <div className="col-md-8">
-                  <p>The Prime Minister's Veterans' Employment Program demonstrates the importance the Government places on raising awareness with employers,
-                     both private and public sector, and of the value and unique experience of our veterans. <Link to="/about">Read more</Link>
-                  </p>
+                  <p>The Prime Minister's Veterans' Employment Program demonstrates the importance the Government places on raising awareness with employers, 
+                     both private and public sector, and of the value and unique experience of our veterans. <a href="#" onClick={ () => this.handleClick() }>{this.state.show ? 'Read less' : 'Read more'}</a></p>                    
+                     <ToggleDisplay show={this.state.show}>
+                     <p>The Australian Defence Force (ADF) invests heavily in its servicemen and women, and they have a broad range of skills and experience. 
+                        Their skill-sets, including proven leadership and problem solving skills, are in strong demand and transfer readily to civilian employment. </p>
+                     <p>Australian businesses of all sizes, and across all industries, have an opportunity to acquire these skilled and capable employees when they
+                        separate from the ADF.  The Program aims to make businesses more aware of the value that veterans can add to their organisations, and as a 
+                        result improve employment opportunities for veterans.</p>
+                    </ToggleDisplay>                                             
                 </div>
               </div>
             </div>
