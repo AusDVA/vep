2ec2272e23eeb25866317b714c2ca952bc56b6ce
Callum McKenna <my.nirvana@gmail.com>
2017-05-25 09:21:02 +1000

    Moved VEP Logo to the left hand margin instead of centered

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import VEPLogo from '../image/banner3.png';
+
 export default class Header extends React.Component {
 
   render(){
     return (
       <div id="header">
-        <header className="newsfeed" id="top">
-          
+        <header className="vep-branding" id="top">
+          <div className="container">
+              <img src={VEPLogo} alt="Veterans' Employment Program"/>
+          </div>
         </header>
       </div>
     );
