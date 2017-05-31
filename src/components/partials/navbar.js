f3488f57614c6bdedb2eaade907ccc4050dc6a1a
Jack <jack.dillon266@gmail.com>
2017-06-01 09:50:28 +1000

    remove non breaking space. Move contact link down. Aria hidden true on items that only show in print.

--- src/components/partials/navbar.js
+++ src/components/partials/navbar.js
@@ -19,8 +19,6 @@ export default class NavBar extends React.Component {
     return false;
   }
 
-
-
   render() {
     return (
       <div className="container hidden-print">
@@ -28,22 +26,14 @@ export default class NavBar extends React.Component {
           <div className="logo col-md-6 col-sm-6 col-xs-12"><img alt="Logo" src={GOVLogo}/></div>
            <div className="navbar-header col-md-1 col-xs-12 text-center">
       <button onClick={this.handleClick}  type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#mobile-nav" aria-expanded="false">
-         <a href="#"><h3 className="mobile-menu text-center">Menu &nbsp;
+         <a href="#"><h3 className="mobile-menu text-center">Menu
               <span className= { this.state.isToggleOn ? "glyphicon glyphicon-chevron-down rotate-down" : "glyphicon glyphicon-chevron-down rotate"}
                     aria-hidden="true"></span></h3></a>
       </button>
        </div>
-           <div className="col-md-6 col-xs-12">
-              <nav className="collapse navbar-collapse text-center" id ="top-nav">
-             <ul className="nav navbar-nav navbar-right">
-               <li><NavLink to="/contact">Contact </NavLink></li>
 
-             </ul>
-           </nav>
-          </div>
         </div>
       <nav >
-
     <div className="collapse navbar-collapse" id="main-nav">
       <ul className="nav navbar-nav text-center">
        <li><NavLink exact to="/">Home</NavLink></li>
@@ -52,28 +42,26 @@ export default class NavBar extends React.Component {
        <li><NavLink to="/ex-service-organisations">Ex-service organisations</NavLink></li>
        <li><NavLink to="/industry-advisory-committee">Industry Advisory Committee</NavLink></li>
        <li><NavLink to="/awards">Awards</NavLink></li>
+       <li><NavLink to="/contact">Contact </NavLink></li>
      </ul>
     </div>
 </nav>
 
 <nav onClick={this.handleClick} className="mobile-nav">
 
-<div className="collapse navbar-collapse" id="mobile-nav">
-<ul className="nav navbar-nav text-center">
- <li><NavLink to="/contact" data-toggle="collapse" data-target="#mobile-nav">Contact </NavLink></li>
- <li><NavLink exact to="/" data-toggle="collapse" data-target="#mobile-nav">Home</NavLink></li>
- <li><NavLink to="/veterans" data-toggle="collapse" data-target="#mobile-nav">Veterans</NavLink></li>
- <li><NavLink to="/employers" data-toggle="collapse" data-target="#mobile-nav">Employers</NavLink></li>
- <li><NavLink to="/ex-service-organisations" data-toggle="collapse" data-target="#mobile-nav">Ex-service organisations</NavLink></li>
- <li><NavLink to="/industry-advisory-committee" data-toggle="collapse" data-target="#mobile-nav">Industry Advisory Committee</NavLink></li>
- <li><NavLink to="/awards" data-toggle="collapse" data-target="#mobile-nav">Awards</NavLink></li>
-</ul>
+  <div className="collapse navbar-collapse" id="mobile-nav">
+    <ul className="nav navbar-nav text-center">
+      <li><NavLink exact to="/" data-toggle="collapse" data-target="#mobile-nav">Home</NavLink></li>
+      <li><NavLink to="/veterans" data-toggle="collapse" data-target="#mobile-nav">Veterans</NavLink></li>
+      <li><NavLink to="/employers" data-toggle="collapse" data-target="#mobile-nav">Employers</NavLink></li>
+      <li><NavLink to="/ex-service-organisations" data-toggle="collapse" data-target="#mobile-nav">Ex-service organisations</NavLink></li>
+      <li><NavLink to="/industry-advisory-committee" data-toggle="collapse" data-target="#mobile-nav">Industry Advisory Committee</NavLink></li>
+      <li><NavLink to="/awards" data-toggle="collapse" data-target="#mobile-nav">Awards</NavLink></li>
+      <li><NavLink to="/contact" data-toggle="collapse" data-target="#mobile-nav">Contact </NavLink></li>
+    </ul>
+   </div>
+ </nav>
 </div>
-</nav>
-      </div>
-
-
-
     );
   }
 }
