1f6836467bb2e591d6a0d3a9821ee42f33628445
Jack <jack.dillon266@gmail.com>
2017-06-02 16:05:25 +1000

    News content, remove unused images.

--- src/components/partials/news.js
+++ src/components/partials/news.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Opera from '../../image/opera-house.jpg';
-import Worker from '../../image/worker.jpg';
+import IAC_Img from "../../image/iac.png";
+import JobActive from "../../image/jobactive.png";
 import ToggleDisplay from "react-toggle-display";
 export default class News extends React.Component {
 
@@ -27,11 +27,12 @@ export default class News extends React.Component {
     			<div className="col-sm-12">
     				<h2>Recent news</h2>
     			</div>
-    			<div className="col-sm-12 col-md-6">
+    			<div className="col-sm-12 col-md-5">
             <section>
     					<div className="row">
-    						<div className="col-sm-4 col-xs-6"><img alt="Opera house" src={Opera}/></div>
-    						<div className="col-sm-8">
+    						<div className="col-sm-4 col-xs-12"><img alt="Group photo of Industry Advisory Committee members." src={IAC_Img}/></div>
+    						<div className="col-sm-12">
+                  <h4 className="news-headline">Industry Advisory Committee's inaugural meeting</h4>
                   <p>The Industry Advisory Committee on Veterans' Employment held its inaugural meeting in Sydney on 31 March 2017.</p>
                   <ToggleDisplay show={this.state.show}>
                   <p>At the meeting, the Committee focused on establishing a number of priority areas of focus. These areas include the availability of data on the employment profile of veterans, improving the transition process from the ADF, translating skills and raising awareness of the value that former members of the ADF offer to Australian businesses.</p>
@@ -45,12 +46,12 @@ export default class News extends React.Component {
               </section>
 
     			</div>
-    			<div className="col-sm-12 col-md-6">
+    			<div className="col-sm-12 col-md-5 col-md-offset-1">
     				<section>
     					<div className="row">
-    						<div className="col-sm-4 col-xs-6"><img alt="Disabled worker" src={Worker}/></div>
-    						<div className="col-sm-8">
-    							<h4 className="news-headline"><a href="#">Helping veterans to find jobs</a></h4>
+    						<div className="col-sm-4 col-xs-12"><img className="text-center" alt="JobActive Logo" src={JobActive}/></div>
+    						<div className="col-sm-12">
+    							<h4 className="news-headline">Helping veterans to find jobs</h4>
     							<p>Jobactive is a board to help veterans to find jobs where ADF experience is valued... <a href="#">Read&nbsp;more</a></p>
     						</div>
     					</div>
