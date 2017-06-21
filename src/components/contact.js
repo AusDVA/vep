16a81221398ef21484ef21e4bf73d1ab56a73999
Jack <jack.dillon266@gmail.com>
2017-06-21 18:04:16 +1000

    Descriptive text for screen reader, contact page.

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,7 +9,8 @@ export default class Contact extends React.Component {
           <div className="col-md-10" id="mainCol">
 
             <h1>Contact</h1>
-            <h2 className="contact-subheader">General enquiries (8am-5pm AEST, Monday-Friday)</h2>
+            <h2 className="contact-subheader" aria-hidden="true">General enquiries (8am-5pm AEST, Monday-Friday)</h2>
+            <h2 className="sr-only">General enquiries (8am-5pm Australian Eastern Standard Time, Monday-Friday)</h2>
             <p><span className="head">Telephone: </span>133 254 </p>
             <p><span className="head">Regional callers: </span>1800 555 254 </p>
             <p><span className="head">International callers: </span>61 2 6289 1133 </p>
