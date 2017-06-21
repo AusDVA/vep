342824eebdf972332bf8c212287d722130ec2c61
Chris Kairys <chris.kairys@dva.gov.au>
2017-06-21 17:26:33 +1000

    copyright page (incomplete)

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -5,8 +5,35 @@ export default class Copyright extends React.Component {
     render() {
         return (
             <div>
+           <section>
+          <div className="body-content container">
+            <div className="row">
+              <div className="col-md-10" id="mainCol">
              <h1>Copyright</h1> 
-            </div>
+            <p>The Commonwealth of Australia owns the copyright of everything included on this website.  All material on this website is provided under a Creative Commons Attribution 3.0 Australia (CC BY 3.0 AU) license, with the exception of:
+<ul><li>The Commonwealth Coat of Arms;</li>
+<li>This Department’s logo;</li>
+<li>Any third party material; and</li>
+<li>Any material protected by trademark.</li></ul>
+You are free to download, display, print and reproduce text content, provided that you attribute the Commonwealth.
+The details of the licence conditions are available on the Creative Commons website at https://creativecommons.org/licenses/by/3.0/au/
+The full legal code for the CC BY 3.0 AU license is available at https://creativecommons.org/licenses/by/3.0/au/legalcode
+Attribution
+CC BY
+Use of all or part of any material on this website must include the following attribution:
+© Commonwealth of Australia 2017
+The terms under which the Commonwealth Coat of Arms can be used are set out on the Department of Prime Minister and Cabinet’s website http://www.dpmc.gov.au/government/commonwealth-coat-arms
+Wherever a third party holds copyright in material presented on this website, the copyright remains with that party.  Their permission may be required to use the material.  The Department has made all reasonable efforts to:
+•	Clearly label material where the copyright is owned by a third party; and
+
+•	Ensure that the copyright owner has consented to this material being presented on this website.
+</p>
+    </div>
+        </div>
+    </div>
+    </section>
+
+    </div>
         );
     }
 }
