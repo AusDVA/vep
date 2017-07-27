8cf7bb20fbda1305317c201b9a564fff55d200be
Jack <jack.dillon266@gmail.com>
2017-07-27 18:26:10 +1000

    Screen-reader text for header email link.

--- src/components/partials/navbar.js
+++ src/components/partials/navbar.js
@@ -28,7 +28,8 @@ export default class NavBar extends React.Component {
       <div>
        <div className="header-message hidden-print" role="alert" id="top">
          <div className="container">
-            This is a trial.<span className="non-mobile-text"> We'd love to hear what you think - please</span> <span className="mobile-text"> Please</span> provide <a href="mailto:veteransemployment@dva.gov.au?subject=Veterans'%20Employment%20Program%20-%20Beta%20Feedback">feedback</a>.
+            This is a trial.<span className="non-mobile-text"> We'd love to hear what you think - please</span> <span className="mobile-text"> Please</span> provide <a href="mailto:veteransemployment@dva.gov.au?subject=Veterans'%20Employment%20Program%20-%20Beta%20Feedback">feedback 
+            <span className="sr-only">(this link will create a new email addressed to veteransemployment@dva.gov.au)</span></a>. 
           </div>
         </div>
       <div className="container hidden-print">
