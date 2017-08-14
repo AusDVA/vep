04d4e87aa3e118e0f1e2e4425a6e49a6485a790a
Jack <jack.dillon266@gmail.com>
2017-08-14 12:48:18 +1000

    VEP-38 - fix single quote in meta tag, fix console error when selecting back to top.

--- src/components/partials/topLink.js
+++ src/components/partials/topLink.js
@@ -3,17 +3,24 @@ import { HashLink as Link } from "react-router-hash-link";
 
 export default class TopLink extends React.Component {
 
-  focus() {
-    this.refs.skipNav.focus();
+   constructor(props) {
+    super(props);
+    this.blur = this.blur.bind(this);
+  }
+
+  blur() {
+   document.activeElement.blur();
   }
 
   render() {
     return (
       <p className="hidden-print">
-        <Link onClick={this.focus} to="#top">Back to top
+        <Link onClick={this.blur} to="#top">Back to top
           <span className="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>
         </Link>
       </p>
     );
   }
 }
+
+
