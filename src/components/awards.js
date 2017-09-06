652b2c5c3ee7f47f3f12ea0b8a4cfb43bb4a2c73
shatheesh <nakesuparan.shatheesh@dva.gov.au>
2017-09-06 11:43:53 +1000

    Awards page Updated

--- src/components/awards.js
+++ src/components/awards.js
@@ -29,7 +29,7 @@ export default class Awards extends React.Component {
 
                  <section id="Apply" className="section-nobreak">
                    <h2>Apply Now</h2>
-                   <p>To submit a nomination, visit the <a target="_blank" href="https://veteransemployment.awardsplatform.com/">Prime Minister’s Veterans’ Employment Awards website.<span className="sr-only">(this link will open in a new tab)</span></a></p>
+                   <p>To submit a nomination, visit the <a target="_blank" href="https://veteransemployment.awardsplatform.com/">Prime Minister’s Veterans’ Employment Awards platform.<span className="sr-only">(this link will open in a new tab)</span></a></p>
                  </section>
 
                  <section id="KeyDates" className="section-nobreak">
@@ -117,7 +117,7 @@ export default class Awards extends React.Component {
                         <p>Nominations must be made in accordance with the Prime Minister's Veterans' Employment Awards <Link to="/termsandconditions#top">Terms and Conditions.<span className="sr-only">Terms and Conditions page inside this website</span></Link></p>
 
                         <h2 id="FAQ">FAQs</h2>
-                        <p>These are answers to <Link to="/faq#top">Frequently Asked Questions<span className="sr-only">Frequently asked questions page inside this website</span></Link> on submitting nominations for the Prime Minister's Veterans' Employment Awards.</p>
+                        <p>These are answers to <Link to="/faq#top">Frequently Asked Questions<span className="sr-only">Frequently asked questions page inside this website</span></Link> on the Prime Minister's Veterans' Employment Awards.</p>
 
                         <h2 id="Contact">Contact</h2>
                         <p>Contact us by emailing <a href="mailto:veteransemployment@dva.gov.au">veteransemployment@dva.gov.au</a></p>
