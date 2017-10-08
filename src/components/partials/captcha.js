8b0a7ce9dbb0984fd9802e18fca0ff2479397556
Jack <jack.dillon266@gmail.com>
2017-10-09 09:30:18 +1100

    captcha logic

--- src/components/partials/captcha.js
+++ src/components/partials/captcha.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import PropTypes from 'prop-types';
 
-const SITE_KEY = "your site key"; 
+const URL = location.protocol + "//" + location.hostname;
+
+if (URL == "http://localhost") {
+  var SITE_KEY = "6Le_bS0UAAAAADQ60ar1SLU-w-Q_pftEV91VVq1z";
+}
+
+else {
+  var SITE_KEY = "6Lc9tC0UAAAAAFnez6vux4tQ36WC8hlJOesHKrEt";
+}
 
 const Captcha = (props) => (
 
   <div className = "col-md-12">
-
     <ReCAPTCHA
       sitekey={SITE_KEY}
       onChange= {response => props.input.onChange(response)}
