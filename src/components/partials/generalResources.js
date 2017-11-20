633abacb363f166a20eb331721608576634a0121
Jack-Dillon <jack.dillon266@gmail.com>
2017-11-20 11:44:14 +1100

    New media release.

--- src/components/partials/generalResources.js
+++ src/components/partials/generalResources.js
@@ -12,9 +12,12 @@ export default class GeneralResourses extends React.Component {
                 <p><a href={process.env.PUBLIC_URL + '/doc/ADF Separations 2012 to 2016.xlsx'} title="ADF Separations 2012 to 2016.xlsx">ADF Separations 2012-2016 by State and Category (XLSX 808 KB)</a></p>
 
                 <h3>Media Releases</h3>
-                <ul className="list-unstyled"> 
-                <li> 6 September 2017 <a target="_blank" href="https://www.pm.gov.au/media/2017-09-06/government-launches-veterans-employment-awards">Government launches Veterans' Employment Awards 
-                    <span className="sr-only">(this link will open in a new tab)</span> </a></li>  
+                <ul className="list-unstyled">
+
+                  <li>17 November 2017 <a target="_blank" href="http://minister.dva.gov.au/media_releases/2017/nov/va154.htm">Supporting veterans’ employment 
+                      <span className="sr-only">(this link will open in a new tab)</span> </a></li>
+                <li> 6 September 2017 <a target="_blank" href="https://www.pm.gov.au/media/2017-09-06/government-launches-veterans-employment-awards">Government launches Veterans' Employment Awards
+                    <span className="sr-only">(this link will open in a new tab)</span> </a></li>
                 <li> 17 June 2017 <a target="_blank" href="http://minister.dva.gov.au/media_releases/2017/jun/va079.htm">More than 1,000 jobs for
                 veterans flagged <span className="sr-only">(this link will open in a new tab)</span> </a></li>
                 <li> 31 March 2017 <a target="_blank" href="http://minister.dva.gov.au/media_releases/2017/mar/va043.htm">First meeting
